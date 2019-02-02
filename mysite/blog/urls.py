b2291aec89808fa207cd3c926480add8142aab62
tusshar05 <tushar.joshi789@gmail.com>
2019-02-02 18:27:38 +0530

    Function based views for comments

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -9,6 +9,10 @@ urlpatterns=[
     re_path(r'^post/new/$',views.CreatePostView.as_view(),name='post_new'),
     re_path(r'^/(?P<pk>\d+)/edit/$',views.PostUpdateView.as_view(),name='post_edit'),
     re_path(r'^post(?P<pk>\d+)/remove/$',views.PostDeleteView.as_view(),name='post_remove'),
-    re_path(r'^drafts/$',views.DraftListView.as_view(),name='post_draft_list')
+    re_path(r'^drafts/$',views.DraftListView.as_view(),name='post_draft_list'),
+    re_path(r'^post/(?P<pk>\d+)/comment/',views.add_comment_to_post,name='add_comment_to_post'),
+    re_path(r'^comment/(?P<pk>\d+)/approve/$',views.comment_approved,name='comment_approved'),
+    re_path(r'^comment/(?P<pk>\d+)/remove/$',views.comment_remove,name='comment_remove'),
+    re_path(r'^post/(?P<pk>\d+)/publish/$',views.post_publish,name='post_publish')
 
 ]
