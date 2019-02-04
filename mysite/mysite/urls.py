a19eaa740a852470c097a9ee0c7489d0c7bc9449
tusshar05 <tushar.joshi789@gmail.com>
2019-02-04 13:18:22 +0530

    Bug Fix Login
    Some Changes in login for django in version 2.1

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
      path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/logout',logout,name='logout',kwargs={'next_page':'/'})
+    
 ]
